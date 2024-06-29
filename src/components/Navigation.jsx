bd9db7834fdbe70cc33978941d96f3015ed970af
Makaosva <maka.osses@gmail.com>
2024-06-28 20:16:27 -0400

    feat/Creacion Navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,24 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function Navigation() {
+  return (
+    <Navbar bg="danger" variant="dark">
+      <Container fluid className="justify-content-start">
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/">
+              Home 🏠
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contacto">
+              Contacto
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Brand>Happy Cake 🥧</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
