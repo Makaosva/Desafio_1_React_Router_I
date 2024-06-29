fb79a5a246e8fdcb3fa2bd5773cff675c4654b77
Makaosva <maka.osses@gmail.com>
2024-06-28 20:29:38 -0400

    feat/Creacion HomePage

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <Container className="text-center">
+      <h1 className="pt-5">
+        Bienvenido a <span className="fw-bold">Happy Cake</span>
+      </h1>
+      <h6> El lugar de los pasteles felices </h6>
+      <img src="./assets/pastel.jepg" alt="" />
+    </Container>
+  );
 }
-
-export default HomePage
+export default HomePage;
